1faa19d61a3962a034fa4ac0c972d1a1da0a6c4b
ddddurk <ddddurk@proton.me>
2023-04-30 05:18:50 -0400

    feat(@ddddurk/cli): \n start commands

--- packages/cli/src/boxVersion.ts
+++ packages/cli/src/boxVersion.ts
@@ -4,6 +4,8 @@ import { version } from "../package.json";
 import { colors, logDdddurk } from "./lib";
 
 export const boxVersion = () => {
+  console.log("\n");
+
   logDdddurk(
     chalk.hex(colors.pink)("v" + version) +
       " @ddddurk/cli initiated."
